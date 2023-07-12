6bc501431262b36a6b01bdb82d31e2e612c604b8
Zack Hanni <zack.hannI@gmail.com>
2023-07-12 11:16:27 -0400

    route pages to home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar/>
+        <HeroImg/>
+    </div>
   )
 }
 
