2ecebc23af33d4a0854420f3f7b8fecb59e75746
Zack Hanni <zack.hannI@gmail.com>
2023-07-13 11:53:54 -0400

    create dataset for work data to auto compile

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,27 +1,22 @@
 import "./WorkCardStyles.css"
 
 import React from 'react'
-import pro1 from "../assets/project1.png"
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom" 
 
-const WorkCard = () => {
-  return <div className="work-container">
-    <h1 className="project-heading">Projects</h1>
-    <div className="project-container">
-        <div className="project-card">
-            <img src={pro1} alt="Work Demo 1"></img>
-            <h2 className="project-title">Project Title</h2>
-            <div className="pro-details">
-                <p>This is the info below the image</p>
-                <div className="pro-btns">
-                    <NavLink to="www.soundslikesoma.com" className="btn">View Live Page</NavLink>
-                    <NavLink to="url.com" className="btn">Source</NavLink>
-                </div>
-            </div>
-        </div>
+const WorkCard = (props) => {
+  return (
+    <div className="project-card">
+      <img src={props.imgsrc} alt="Website Demo" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+          <p>{props.text}</p>
+          <div className="pro-btns">
+              <NavLink to={props.view} className="btn">View Live Page</NavLink>
+              <NavLink to={props.source} className="btn">Source</NavLink>
+          </div>
+      </div>
     </div>
-
-  </div>
+  )
 }
 
 export default WorkCard
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,30 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "This is the bottom text below the first card",
+        view: "apple.com",
+        source: "github.com"
+    },
+    {
+        imgsrc: pro2,
+        title: "Website Demo 2",
+        text: "This is the bottom text below the second card on the list. Here, i will put more text once I know which project im going to post here",
+        view: "google.com",
+        source: "github.com"
+    },
+    {
+        imgsrc: pro3,
+        title: "Recording Studio Website",
+        text: "This is a website i created for a local thriving business in south Philadelphia. It is a recording studio that offers various recording services, blogs, and music demos",
+        view: "https://www.soundslikesoma.com/",
+        source: "github.com"
+    },
+    
+];
+
+export default ProjectCardData;
