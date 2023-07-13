69848ef3589dbeaa2c62f54d101a42f3d6c5f2d1
Zack Hanni <zack.hannI@gmail.com>
2023-07-13 10:24:07 -0400

    create pricing cards

---
+++ src/components/PricingCard.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom"
+import "./PricingCardStyles.css"
+
+import React from 'react'
+
+const PricingCard = () => {
+    return <div className="pricing">
+        <div className="card-container">
+            <div className="card">
+                <h3>- Basic -</h3>
+                <span className="bar"></span>
+                <p className="btc">$ 100</p>
+                <p>- 3 Days -</p>
+                <p>- 3 Pages -</p>
+                <p>- Featured - </p>
+                <p>- Responsive Design - </p>
+                <Link to="/contact" className="btn">Purchase Now</Link>
+            </div>
+
+            <div className="card">
+                <h3>- Standard -</h3>
+                <span className="bar"></span>
+                <p className="btc">$ 200</p>
+                <p>- 3 Days -</p>
+                <p>- 3 Pages -</p>
+                <p>- Featured - </p>
+                <p>- Responsive Design - </p>
+                <Link to="/contact" className="btn">Purchase Now</Link>
+            </div>
+
+            <div className="card">
+                <h3>- Business -</h3>
+                <span className="bar"></span>
+                <p className="btc">$ 300</p>
+                <p>- 3 Days -</p>
+                <p>- 3 Pages -</p>
+                <p>- Featured - </p>
+                <p>- Responsive Design - </p>
+                <Link to="/contact" className="btn">Purchase Now</Link>
+            </div>
+        </div>
+    </div>
+}
+
+export default PricingCard
