b0da4ebe2ba5974ce87a9430e4aaa0a4b7072afb
Zack Hanni <zack.hannI@gmail.com>
2023-07-13 10:24:28 -0400

    add heroimg2 text to other pages

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const About = () => {
   return (
     <div>
         <Navbar/>
-        
+        <HeroImg2 heading="ABOUT." text="I'm your friendly neighborhood front-end developer." />
         <Footer/>
     </div>
   )
