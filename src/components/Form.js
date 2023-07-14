eb9c9b35cf3812e7a6fd02c722adcd6b25d99d87
Zack Hanni <zack.hannI@gmail.com>
2023-07-14 10:23:27 -0400

    create email form in contact

---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import "./FormStyles.css"
+
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className="form">
+        <form>
+            <label>Your Name</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="text"></input>
+            <label>Subject</label>
+            <input type="text"></input>
+            <label>Message</label>
+            <textarea rows="6" placeholder="What's on your mind?" />
+            <button className="btn">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
