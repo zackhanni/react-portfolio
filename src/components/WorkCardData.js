d2ba4ce374622a1ef10f299d22fec12965dcb665
Zack Hanni <zack.hannI@gmail.com>
2023-07-17 16:59:36 -0400

    soma website card

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,18 +1,18 @@
-import pro1 from "../assets/project1.png"
+import pro1 from "../assets/soma-website.png"
 import pro2 from "../assets/project2.png"
 import pro3 from "../assets/project3.png"
 
 const ProjectCardData = [
     {
         imgsrc: pro1,
-        title: "Ecommerce Website Design",
-        text: "This is the bottom text below the first card",
-        view: "apple.com",
-        source: "github.com"
+        title: "Recording Studio Website",
+        text: "I was hired by Soma to create a responsive service based website with SEO and load speed tests in mind.",
+        view: "https://www.soundslikesoma.com/",
+        source: "https://github.com/zackhanni/soundslikesoma"
     },
     {
         imgsrc: pro2,
-        title: "Website Demo 2",
+        title: "Ecommerce Website Design",
         text: "This is the bottom text below the second card on the list. Here, i will put more text once I know which project im going to post here",
         view: "google.com",
         source: "github.com"
