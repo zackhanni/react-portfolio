2ae92ed04015c00e442467b9adfe08487fb442ed
Zack Hanni <zack.hannI@gmail.com>
2023-07-17 17:11:40 -0400

    update work cards

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,28 +1,28 @@
 import pro1 from "../assets/soma-website.png"
-import pro2 from "../assets/project2.png"
-import pro3 from "../assets/project3.png"
+import pro2 from "../assets/fitness-gym.png"
+import pro3 from "../assets/project1.png"
 
 const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Recording Studio Website",
-        text: "I was hired by Soma to create a responsive service based website with SEO and load speed tests in mind.",
+        text: "I was hired by Soma to create a responsive service based website with SEO and load speed tests in mind. I deployed the page on netlify and handled DNS",
         view: "https://www.soundslikesoma.com/",
         source: "https://github.com/zackhanni/soundslikesoma"
     },
     {
         imgsrc: pro2,
-        title: "Ecommerce Website Design",
-        text: "This is the bottom text below the second card on the list. Here, i will put more text once I know which project im going to post here",
-        view: "google.com",
-        source: "github.com"
+        title: "Fitness Gym",
+        text: "I built an example gym website as a way to practice coding with typescript and react. I'm eager to learn more modern and optimised ways to code websites!",
+        view: "https://github.com/zackhanni/fitness-gym",
+        source: "https://github.com/zackhanni/fitness-gym"
     },
     {
         imgsrc: pro3,
-        title: "Recording Studio Website",
-        text: "This is a website i created for a local thriving business in south Philadelphia. It is a recording studio that offers various recording services, blogs, and music demos",
-        view: "https://www.soundslikesoma.com/",
-        source: "github.com"
+        title: "Ecommerce Website Design",
+        text: "A product website for a clothing company selling a variety of clothing. Complete with item descriptions, size and color options, and a checkout page.",
+        view: "https://github.com/zackhanni/ecommerce-website-demo",
+        source: "https://github.com/zackhanni/ecommerce-website-demo"
     },
     
 ];
