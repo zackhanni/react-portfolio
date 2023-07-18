5c83d93bd93b074e9a8b8a37a027229ea94cd03d
Zack Hanni <zack.hannI@gmail.com>
2023-07-17 23:12:36 -0400

    remove pricing card

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,16 +3,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
-import PricingCard from '../components/PricingCard'
+// import PricingCard from '../components/PricingCard'
 import Work from '../components/Work'
 
 const Project = () => {
   return (
     <div>
         <Navbar/>
-        <HeroImg2 heading="PROJECTS." text="Some  of my recent works" />
+        <HeroImg2 heading="PROJECTS." text="Some of my recent works" />
         <Work />
-        <PricingCard />
+        {/* <PricingCard /> */}
         <Footer/>
     </div>
   )
