311e9c053749dec9dd4d1d6127aa1900d49cf029
Zack Hanni <zack.hannI@gmail.com>
2023-08-01 12:04:36 -0400

    fitness website is incomplete and should not be up

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,34 +1,34 @@
-import pro1 from "../assets/soma-website.webp"
-import pro2 from "../assets/fitness-gym.webp"
-import pro3 from "../assets/project3.png"
-import pro4 from "../assets/UX-design-demo.webp"
+import soma from "../assets/soma-website.webp"
+import fitness from "../assets/fitness-gym.webp"
+import ecommerce from "../assets/project3.png"
+import uxdesign from "../assets/UX-design-demo.webp"
 import emointel from "../assets/emo-intel.png"
 import calculator from "../assets/calculator.png"
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
+        imgsrc: soma,
         title: "Recording Studio Website",
         text: "I was hired by Soma to create a responsive service based website with SEO and load speed tests in mind. I deployed the page on netlify and handled DNS",
         view: "https://www.soundslikesoma.com/",
         source: "https://github.com/zackhanni/soundslikesoma"
     },
+    // {
+    //     imgsrc: fitness,
+    //     title: "Fitness Studio",
+    //     text: "I built website for a fitness studio with TypeScript and React.js ",
+    //     view: "https://github.com/zackhanni/fitness-gym",
+    //     source: "https://github.com/zackhanni/fitness-gym"
+    // },
     {
-        imgsrc: pro2,
-        title: "Fitness Studio",
-        text: "I built an example gym website as a way to practice coding with typescript and react. I'm eager to learn more modern and optimised ways to code websites!",
-        view: "https://github.com/zackhanni/fitness-gym",
-        source: "https://github.com/zackhanni/fitness-gym"
-    },
-    {
-        imgsrc: pro3,
+        imgsrc: ecommerce,
         title: "Ecommerce Website Design",
         text: "A product website for a clothing company selling a variety of clothing. Complete with item descriptions, size and color options, and a checkout page.",
         view: "https://zack-ecommerce.netlify.app/",
         source: "https://github.com/zackhanni/ecommerce-website-demo"
     },
     {
-        imgsrc: pro4,
+        imgsrc: uxdesign,
         title: "UX and UI Design",
         text: "I have experience designing products and apps with Figma. I have a seperate portfolio of designs and case studies featured on Dribble.com",
         view: "https://dribbble.com/zackhanni",
